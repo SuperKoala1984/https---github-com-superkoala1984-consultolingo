8976edf35c5f037f06093cfca4a2935175f412fc
JasminvMt <165830581+JasminvMt@users.noreply.github.com>
2024-05-11 21:55:09 +0000

    Comments 2

--- Hello.py
+++ Hello.py
@@ -1,4 +1,7 @@
 import streamlit as st
+
+#Startseite der Application, welche den User willkommen heisst, den Nutzen der Application erwähnt und auf das neuste Quiz verweisst
+
 st.title("Welcome to Consultolingo!")
 st.write("We prepare you for your consulting case study interview with interactive quizzes!")
 st.write("Newest Quiz: European Population Quiz")
--- pages/Background Information.py
+++ pages/Background Information.py
@@ -1,4 +1,7 @@
 import streamlit as st
+
+#Seite enthält Hintergrundinformation zu den Erstellern der Applikation und dem Ursprung der Daten
+
 st.title("Background Information")
 st.write("This app was created by students of the University of St. Gallen as part of the course 'Grundlagen und Methoden der Informatik für Wirtschaftswissenschaften'.")
 st.write("All data used originates from Eurostat.")
