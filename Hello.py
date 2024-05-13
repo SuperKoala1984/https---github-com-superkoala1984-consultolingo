1a9996fc16f98a87c6ba280fda358367472902b3
JasminvMt <165830581+JasminvMt@users.noreply.github.com>
2024-05-13 12:32:28 +0000

    Titel page

--- Hello.py
+++ Hello.py
@@ -4,4 +4,5 @@ import streamlit as st
 
 st.title("Welcome to Consultolingo!")
 st.write("We prepare you for your consulting case study interview with interactive quizzes!")
+st.write("Our quizzes prepare you for market sizing exercises in your consulting case study interview.  The range of quizzes is regularly expanded to ensure that you can continuously improve your skills.")
 st.write("Newest Quiz: European Population Quiz")
